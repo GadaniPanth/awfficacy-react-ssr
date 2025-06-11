1c09185f6a42bb10628028513e6604918689dad8
Parmaryash-7 <parmars4679@gmail.com>
2025-06-11 18:13:05 +0530

    pending

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,8 +3,22 @@ import '../styles/Landing.css'
 
 const Landing = () => {
   return (
-    <div className="main-container section-padding">
-         <h1>asdfds</h1>
+    <div className="awfficacy_wrapper">
+      <div className="main-container section-padding">
+        <div className="relative">
+          <div className="awfficacy_blue_box">
+            <div className="awfficacy_blue_box_heading">
+              <h1 className="font_56 white-color">
+                Building awesome office <br/>
+                with efficacy
+              </h1>
+            </div>
+            <div className="awfficacy_blue_box_img">
+              <img src=""/>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
