673dd6175c0423068b00c7d203c740203c4782e1
Parmaryash-7 <parmars4679@gmail.com>
2025-06-11 19:02:41 +0530

    pending

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,17 +4,34 @@ import "../styles/Landing.css";
 const Landing = () => {
   return (
     <div className="awfficacy_wrapper">
-      <div className="main-container section-padding">
+      <div className="section-1 main-container section-padding-4rem">
         <div className="relative">
-          <div className="awfficacy_blue_box">
+          <div className="awfficacy_blue_box alc j-c-sb">
             <div className="awfficacy_blue_box_heading">
               <h1 className="font_56 white-color">
-                Building awesome office <br/>
+                Building awesome office <br />
                 with efficacy
               </h1>
             </div>
             <div className="awfficacy_blue_box_img">
-              <img src=""/>
+              <img src="./landingPage.svg" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section-2 main-container section-padding-4rem">
+        <div className="grid-two">
+          <div className="section-2-left">
+            <div className="section-2-img-details flex-row">
+              <img src="./Awfficacy Logo 1.svg" alt="" />
+                <button>
+                    <p>Know More</p>
+                </button>
+            </div>
+          </div>
+          <div className="section-2-right">
+            <div className="section-2-img-details flex-row">
+              <img src="./global.svg" alt="" />
             </div>
           </div>
         </div>
