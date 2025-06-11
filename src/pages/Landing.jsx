5dda4ce1242122e565660ae93ad0742b8f6020d7
GadaniPanth <gadanipanth@gmail.com>
2025-06-11 17:57:09 +0530

    added Fonts

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,12 +2,9 @@ import React from "react";
 
 const Landing = () => {
   return (
-    <div>
-      <h2>Landing Page</h2>
-      <p>
-        This is the landing page of the website, shown to first-time visitors.
-      </p>
-    </div>
+    <>
+      <main></main>
+    </>
   );
 };
 
