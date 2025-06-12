5910319bba3dbfd39bd2635d159f064ec0c66316
Parmaryash-7 <parmars4679@gmail.com>
2025-06-12 11:36:31 +0530

    landing web complete responsive pending

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Landing.css";
+import { Link } from "react-router";
 
 const Landing = () => {
   return (
@@ -8,7 +9,7 @@ const Landing = () => {
         <div className="relative">
           <div className="awfficacy_blue_box alc j-c-sb">
             <div className="awfficacy_blue_box_heading">
-              <h1 className="font_56 white-color">
+              <h1 className="font_56 white-color fw_700">
                 Building awesome office <br />
                 with efficacy
               </h1>
@@ -22,16 +23,119 @@ const Landing = () => {
       <div className="section-2 main-container section-padding-4rem">
         <div className="grid-two">
           <div className="section-2-left">
-            <div className="section-2-img-details flex-row">
-              <img src="./images/Awfficacy Logo 1.svg" alt="Awfficacy Logo" />
-              <button>
-                <p>Know More</p>
-              </button>
+            <div className="inner-flex gap_spacing">
+              <div className="section-2-img-details flex-row j-c-sb alc">
+                <img src="./images/Awfficacy Logo 1.svg" alt="Awfficacy Logo" />
+                <div className="btn-know_icon">
+                  <Link href="/">
+                    <div className="btn-know flex-row alc">
+                      <p className="font_24 fw_bold"> Know More</p>
+                      <div className="arrow-svg">
+                        <span className="material-symbols-outlined">
+                          near_me
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              <div className="section-2-heading_list inner-flex gap_spacing">
+                <h2 className="font_32 fw_700">Make In India Advisory</h2>
+                <div className="inner-flex gap_spacing">
+                  <div className="inner-flex">
+                    <p className="fw_bold font_24">
+                      a. Central government schemes
+                    </p>
+                    <p className="fw_400 font_24">
+                      (i) Production Linked Incentive (PLI) schemes{" "}
+                    </p>
+                    <p className="fw_400 font_24">
+                      (ii) Other Central government schemes
+                    </p>
+                  </div>
+                  <div className="state_paragraph">
+                    <p className="fw_bold font_24">
+                      b. State government schemes
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <div className="section-2-right">
-            <div className="section-2-img-details flex-row">
-              <img src="./global.svg" alt="" />
+            <div className="inner-flex gap_spacing">
+              <div className="section-2-img-details flex-row j-c-sb alc">
+                <img src="./global.svg" alt="global" />;
+                <div className="btn-know_icon">
+                  <Link href="/">
+                    <div className="btn-know flex-row alc">
+                      <p className="font_24 fw_bold"> Know More</p>
+                      <div className="arrow-svg">
+                        <span className="material-symbols-outlined">
+                          near_me
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+              <div className="section-2-heading_list inner-flex gap_spacing">
+                <h2 className="font_32 fw_700">Global Outsourcing</h2>
+                <div className="inner-flex gap_spacing">
+                  <div className="inner-flex">
+                    <p className="fw_bold font_24">a. Countries</p>
+                    <p className="fw_400 font_24">(i) Australia</p>
+                    <p className="fw_400 font_24">(ii) United Kingdom</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-3 main-container section-padding-4rem">
+        <div className="section-2-left">
+          <div className="inner-flex gap_spacing">
+            <div className="section-2-img-details flex-row j-c-sb alc">
+              <img src="./images/AWF cap.svg" alt="Awfficacy Logo" />
+              <div className="btn-know_icon">
+                <Link href="/">
+                  <div className="btn-know flex-row alc">
+                    <p className="font_24 fw_bold"> Know More</p>
+                    <div className="arrow-svg">
+                      <span className="material-symbols-outlined">near_me</span>
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </div>
+            <div className="section-2-heading_list inner-flex gap_spacing">
+              <h2 className="font_32 fw_700">Investment Banking</h2>
+              <div class="grid-three">
+                <div className="inner-flex">
+                  <div className="inner-flex">
+                    <p className="fw_bold font_24">
+                      a. Corporate Finance Advisory
+                    </p>
+                    <p className="fw_400 font_24">(i) Due diligence</p>
+                    <p className="fw_400 font_24">
+                      (ii) Financial modelling & valuations
+                    </p>
+                    <p className="fw_400 font_24">(iii) IPO readiness</p>
+                  </div>
+                </div>
+                <div class="inner-flex">
+                  <div className="state_paragraph">
+                    <p className="fw_bold font_24">b. Deal Management</p>
+                    <p className="fw_400 font_24">(i) Fundraising advisory </p>
+                    <p className="fw_400 font_24">
+                      (ii) Transactional advisory
+                    </p>
+                  </div>
+                </div>
+                <p className="fw_bold font_24">c. Mutual Fund advisory</p>
+              </div>
             </div>
           </div>
         </div>
