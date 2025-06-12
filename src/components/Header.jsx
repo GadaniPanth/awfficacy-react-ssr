358b687741ea6ca57b06562e910c4de62ee1239b
GadaniPanth <gadanipanth@gmail.com>
2025-06-12 11:39:34 +0530

    Added header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 const Header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const [isMobNavOpen, setIsMobNavOpen] = useState(false);
+
+  useEffect(() => {
+    if (isDropdownOpen) {
+      document.body.style.overflowX = "hidden";
+    } else {
+      document.body.style.overflowX = "auto";
+    }
+  }, [isMobNavOpen]);
 
   return (
     <>
@@ -27,74 +37,156 @@ const Header = () => {
               <img src="./images/Awfficacy Logo 1.svg" alt="Awfficacy Logo" />
             </Link>
           </div>
-          <ul className="navbar">
-            <li className="link-font-size">
-              <Link to="/home">
-                <p>Home</p>
-              </Link>
-            </li>
-            <li className="link-font-size">
-              <Link to="/who-we-are">
-                <p>Who we are</p>
-              </Link>
-            </li>
-            <li className="link-font-size">
-              <Link to="/what-we-do">
-                <p>What we do</p>
-              </Link>
-            </li>
-            <li className="link-font-size">
-              <Link to="/clientele">
-                <p>Clientele</p>
-              </Link>
-            </li>
-            <li className="link-font-size">
-              <Link to="/careers">
-                <p>Careers</p>
-              </Link>
-            </li>
-            <li className="relative">
-              <div
-                className="dropdown link-font-size"
-                onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-              >
-                <p>Group Companies</p>
-                <span class="material-symbols-outlined">
-                  keyboard_arrow_down
-                </span>
-              </div>
-              {isDropdownOpen && (
-                <div className="dropdown-container">
-                  <div className="link-font-size dropdown-item">
-                    <Link href="/awfficacy-global">
-                      <p>Awfficacy Global</p>
-                    </Link>
+          <div className="navbar-horizontal">
+            <ul className="navbar ">
+              <li className="link-font-size">
+                <Link to="/home">
+                  <p>Home</p>
+                </Link>
+              </li>
+              <li className="link-font-size">
+                <Link to="/who-we-are">
+                  <p>Who we are</p>
+                </Link>
+              </li>
+              <li className="link-font-size">
+                <Link to="/what-we-do">
+                  <p>What we do</p>
+                </Link>
+              </li>
+              <li className="link-font-size">
+                <Link to="/clientele">
+                  <p>Clientele</p>
+                </Link>
+              </li>
+              <li className="link-font-size">
+                <Link to="/careers">
+                  <p>Careers</p>
+                </Link>
+              </li>
+              <li className="relative">
+                <div
+                  className="dropdown link-font-size"
+                  onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                >
+                  <p>Group Companies</p>
+                  <span class="material-symbols-outlined">
+                    keyboard_arrow_down
+                  </span>
+                </div>
+                {isDropdownOpen && (
+                  <div className="dropdown-container">
+                    <div className="link-font-size dropdown-item">
+                      <Link href="/awfficacy-global">
+                        <p>Awfficacy Global</p>
+                      </Link>
+                    </div>
+                    <div className="link-font-size dropdown-item">
+                      <Link href="/awfficacy-capital">
+                        <p>Awfficacy Capital</p>
+                      </Link>
+                    </div>
+                    <div className="link-font-size dropdown-item">
+                      <Link href="/scaleAx">
+                        <p>ScaleAx</p>
+                      </Link>
+                    </div>
                   </div>
-                  <div className="link-font-size dropdown-item">
-                    <Link href="/awfficacy-capital">
-                      <p>Awfficacy Capital</p>
-                    </Link>
-                  </div>
-                  <div className="link-font-size dropdown-item">
-                    <Link href="/scaleAx">
-                      <p>ScaleAx</p>
-                    </Link>
+                )}
+              </li>
+            </ul>
+            <div className="btn">
+              <Link href="/contact">
+                <div className="contact-button">
+                  Contact Us
+                  <div className="arrow-svg">
+                    <img src="./images/arrow.svg" alt="" />
                   </div>
                 </div>
-              )}
-            </li>
-          </ul>
-          <div className="btn">
-            <Link href="/contact">
-              <div className="contact-button">
-                Contact Us
-                <div className="arrow-svg">
-                  <img src="./images/arrow.svg" alt="" />
-                </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </div>
+          {isMobNavOpen && (
+            <nav className="navbar-mob">
+              <div className="nav-logo">
+                <Link href="/">
+                  <img
+                    src="./images/Awfficacy Logo 1.svg"
+                    alt="Awfficacy Logo"
+                  />
+                </Link>
+              </div>
+              <ul className="navbar">
+                <li className="link-font-size">
+                  <Link to="/home">
+                    <p>Home</p>
+                  </Link>
+                </li>
+                <li className="link-font-size">
+                  <Link to="/who-we-are">
+                    <p>Who we are</p>
+                  </Link>
+                </li>
+                <li className="link-font-size">
+                  <Link to="/what-we-do">
+                    <p>What we do</p>
+                  </Link>
+                </li>
+                <li className="link-font-size">
+                  <Link to="/clientele">
+                    <p>Clientele</p>
+                  </Link>
+                </li>
+                <li className="link-font-size">
+                  <Link to="/careers">
+                    <p>Careers</p>
+                  </Link>
+                </li>
+                <li className="relative" style={{ width: "100%" }}>
+                  <div
+                    className="dropdown link-font-size"
+                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                  >
+                    <p>Group Companies</p>
+                    <span class="material-symbols-outlined">
+                      keyboard_arrow_down
+                    </span>
+                  </div>
+                  {isDropdownOpen && (
+                    <div className="dropdown-container">
+                      <div className="link-font-size dropdown-item">
+                        <Link href="/awfficacy-global">
+                          <p>Awfficacy Global</p>
+                        </Link>
+                      </div>
+                      <div className="link-font-size dropdown-item">
+                        <Link href="/awfficacy-capital">
+                          <p>Awfficacy Capital</p>
+                        </Link>
+                      </div>
+                      <div className="link-font-size dropdown-item">
+                        <Link href="/scaleAx">
+                          <p>ScaleAx</p>
+                        </Link>
+                      </div>
+                    </div>
+                  )}
+                </li>
+              </ul>
+              <div className="btn">
+                <Link href="/contact">
+                  <div className="contact-button">
+                    Contact Us
+                    <div className="arrow-svg">
+                      <img src="./images/arrow.svg" alt="" />
+                    </div>
+                  </div>
+                </Link>
+              </div>
+            </nav>
+          )}
         </nav>
+
         <hr
           style={{ width: "90%", borderColor: "#002565", borderWidth: "1px" }}
         />
