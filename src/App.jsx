8d90d076b4df25ad1c18a096d8e9c8f7a309c4af
GadaniPanth <gadanipanth@gmail.com>
2025-06-13 17:11:25 +0530

    Swiper Pending

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import What_we_do from "./pages/What-we-do";
 import "./styles/App.css";
+import Who_we_are from "./pages/Who-we-are";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path="/" element={<Landing />} />
           <Route path="/home" element={<Home />} />
           <Route path="/what-we-do" element={<What_we_do />} />
+          <Route path="/who-we-are" element={<Who_we_are />} />
         </Routes>
       </main>
       <Footer />
