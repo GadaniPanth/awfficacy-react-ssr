e6a9e08e4290675b1752e2881ed4d0779231fe66
Parmaryash-7 <parmars4679@gmail.com>
2025-06-14 12:06:16 +0530

    contact us pending

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import Who_we_are from "./pages/Who-we-are";
 import What_we_do from "./pages/What-we-do";
+import Contact_us from "./pages/Contact-Us";
 import "./styles/App.css";
 import Career from "./pages/Career";
 
@@ -25,6 +26,7 @@ const App = () => {
           <Route path="/home" element={<Home />} />
           <Route path="/who-we-are" element={<Who_we_are />} />
           <Route path="/what-we-do" element={<What_we_do />} />
+          <Route path="/contact" element={<Contact_us />} />
           <Route path="/careers" element={<Career />} />
         </Routes>
       </main>
---
+++ src/pages/Clientele.jsx
@@ -0,0 +1,13 @@
+
+import "../styles/Clientele.css";
+import { Link } from "react-router";
+
+const Clientele = () => {
+
+
+    return (
+    <>asdf</>
+  );
+};
+
+export default Clientele;
